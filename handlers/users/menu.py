09085d630a6d24197baa0ef8fb11b84ccc767417
Raxmatilla <isroilovrahmatilla9@gmail.com>
2024-10-15 11:39:19 +0500

    low time to send

--- handlers/users/menu.py
+++ handlers/users/menu.py
@@ -11,18 +11,19 @@ load={
     "Rezyume":3
     }
 txt={"Menu":"","BIO":"","Link":"","Rezyume":""}
-txt["BIO"] = "<b>Ismi-sharifi: </b>Otajon Bozorboyev\n"   
-txt["BIO"] += "<b>Tug'ilgan yili va joyi:</b> 8-yanvar 1999-yil Jizzax viloyati, Jizzax shahri.\n"
-txt["BIO"] += "<b>Ta'limi:</b> Toshkent temir yo'l transport kasb-hunar kolleji Buxgalteriya hisobi va audit (2015-2018);\n"
-txt["BIO"] += "<b>Ish tajribasi: </b>Jizzax temir yo'l masofasi xodimlar bo'limi ish o'rganuvchisi (2018-2019);\n"
-txt["BIO"] += "Jizzax temir yo'l masofasi xodimlar bo'limi nazoratchisi (2019-2023);\n"
-txt["BIO"] += "Astro Education o'quv markazi dasturlash kursi mentori (2023-hozirgacha)\n"
-txt["BIO"] += "<b>Texnik ko'nikmalari: \n</b>"
-txt["BIO"] += "C, Python, Django, Django Rest, SQLite, PostgreSQL, MySQL, Git, GitHub, HTML, CSS, Telegram Bot, Adobe Photoshop, Web Scraping, Parsing, Microsoft Office(Word, Excel, Power Point, Paint, va h.k.lar)\n\n"
-txt["BIO"] += "<b>Tillar: \n</b>"
-txt["BIO"] += "O'zbek tili (Ona tili);\n"
-txt["BIO"] += "Ingliz tili (B2);\n"
-txt["BIO"] += "Arab tili (O'qiy olish)."
+msg = "<b>Ismi-sharifi: </b>Otajon Bozorboyev\n"   
+msg += "<b>Tug'ilgan yili va joyi:</b> 8-yanvar 1999-yil Jizzax viloyati, Jizzax shahri.\n"
+msg += "<b>Ta'limi:</b> Toshkent temir yo'l transport kasb-hunar kolleji Buxgalteriya hisobi va audit (2015-2018);\n"
+msg += "<b>Ish tajribasi: </b>Jizzax temir yo'l masofasi xodimlar bo'limi ish o'rganuvchisi (2018-2019);\n"
+msg += "Jizzax temir yo'l masofasi xodimlar bo'limi nazoratchisi (2019-2023);\n"
+msg += "Astro Education o'quv markazi dasturlash kursi mentori (2023-hozirgacha)\n"
+msg += "<b>Texnik ko'nikmalari: \n</b>"
+msg += "C, Python, Django, Django Rest, SQLite, PostgreSQL, MySQL, Git, GitHub, HTML, CSS, Telegram Bot, Adobe Photoshop, Web Scraping, Parsing, Microsoft Office(Word, Excel, Power Point, Paint, va h.k.lar)\n\n"
+msg += "<b>Tillar: \n</b>"
+msg += "O'zbek tili (Ona tili);\n"
+msg += "Ingliz tili (B2);\n"
+msg += "Arab tili (O'qiy olish)."
+txt["BIO"]=msg
 @dp.callback_query_handler(in_list=menu)
 async def menu_handler(call:CallbackQuery):
     #await call.message.delete()
